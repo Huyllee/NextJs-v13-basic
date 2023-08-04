a075747b776b3acbf165894d9491b515d6cef4d2
Huyllee <moriarty1807@gmail.com>
2023-08-04 21:47:12 +0700

    upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
-
+import "@/styles/app.css";
 
 export default function Home() {
-  return (  
+  return (
     <div>
-    <ul>
-    <li><a href='/facebook' />facebook</li>
-    <li><a href='/youtube' />youtube</li>
-    <li><a href='/tiktok' />tiktok</li>
-    </ul>
+      <ul>
+        <li className="red">
+          <a href="/facebook">facebook</a>
+        </li>
+        <li>
+          <a href="/youtube">youtube</a>
+        </li>
+        <li>
+          <a href="/tiktok">tiktok</a>
+        </li>
+      </ul>
     </div>
-
-  )
+  );
 }
