c608bc6323ad5da2ce764336936b0f6f9f3e31fd
Huyllee <moriarty1807@gmail.com>
2023-08-07 17:39:32 +0700

    fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
+"use client"; //useEffect chay o client
 import styles from "@/styles/app.module.css";
+import Link from "next/link";
 import UserTable from "../components/Table";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:8000/blogs");
+      const data = await res.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="my-3">
         <ul>
-          <li className={styles["red"]}>
-            <a href="/facebook">facebook</a>
+          <li>
+            <Link href="/facebook">facebook</Link>
           </li>
           <li>
-            <a href="/youtube">youtube</a>
+            <Link href="/youtube">youtube</Link>
           </li>
           <li>
-            <a href="/tiktok">tiktok</a>
+            <Link href="/tiktok">tiktok</Link>
           </li>
         </ul>
       </div>
